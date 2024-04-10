6d3bce51290969c2a542d7436b6d87a49e10dac2
Fedonin12 <165668225+Fedonin12@users.noreply.github.com>
2024-04-10 16:15:12 +0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+let audio = document.querySelector('.audio'),
+    butS = document.querySelector('.logo')
+
+butS.addEventListener('click' , e => {
+    butS.classList.toggle('paused')
+    if (audio.paused){
+        audio.play()
+    }
+    else{
+        audio.pause()
+    }
+})
